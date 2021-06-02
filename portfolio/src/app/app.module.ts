528f8cc0c6bdae417730378996b33059058c8f9f
Abel075 <abeld.correa@gmail.com>
2021-06-02 17:01:11 -0300

    frontend

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    SidebarComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+
+})
+export class AppModule { }
